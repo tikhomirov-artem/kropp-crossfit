e40cab4d2f0f976c154190a057b2982f24090d65
tikhomirovofficial <arteom.tixomirow@yandex.ru>
2025-03-12 22:59:39 +0300

    sections locations, family: adaptive, slider

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const swiper = new Swiper(".family-gallery", {});
+    const swiper = new Swiper(".swiper", {
+        spaceBetween: 25,
+        centered: true,
+        grabCursor: true,
+        loop: true,
+        slidesPerView: "auto"
+    })
 })
 
 
